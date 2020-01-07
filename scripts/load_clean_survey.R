2fc0ca937ca2eb043e52e2cee817989e3259f2e0
Martin Gleason <officergleason@gmail.com>
2020-01-06 20:09:17 -0600

    survey questions found.

--- scripts/load_clean_survey.R
+++ scripts/load_clean_survey.R
@@ -11,3 +11,13 @@ survey <- survey[-c(2), ]
 glimpse(survey)
 
 
+response_phone <- survey %>% filter(!is.na(Q14))
+
+survey$Q1
+
+questions <- survey[1, ]
+
+
+#names(survey) <- as.character(questions)
+
+
